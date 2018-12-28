37d4f7823f905d62a3db1e7610d23715fd841cba
Joel Parker Henderson <joel@joelparkerhenderson.com>
2018-12-27 20:25:51 -0800

    Fix indent

--- tasks/now_string/rust/src/main.rs
+++ tasks/now_string/rust/src/main.rs
@@ -2,8 +2,8 @@ extern crate chrono;
 use chrono::prelude::*;
 
 fn main() {
-  let f = "%Y-%m-%dT%H:%M:%S.%fZ";
-  let d: DateTime<Utc> = Utc::now();
-  let s = d.format(f);
-  println!("{}", s); 
+    let f = "%Y-%m-%dT%H:%M:%S.%fZ";
+    let d: DateTime<Utc> = Utc::now();
+    let s = d.format(f);
+    println!("{}", s); 
 }
